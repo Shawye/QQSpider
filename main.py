55c2cc536dd3fbdc50e95f7734d922dd4d3ca560
96486d9b <96486d9b@gmail.com>
2018-02-20 14:08:14 +0800

    rewrite

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
 
 
     qqspider.init()
-    # qqspider.get_friends_list()
+    qqspider.get_friends_list()
     # qqspider.get_all_friends_contents()
     qqspider.get_given_friend_contents(['601841082'])
     qqspider.get_shuoshuo(['601841082'])
